1aa420cfe5d73713a2038236a6c4e018aeb6e11b
Nekmo <contacto@nekmo.com>
2015-04-26 01:16:54 +0200

    Initial commit for random plugin.

---
+++ setup.py
@@ -0,0 +1,66 @@
+# stevedore/example/setup.py
+from setuptools import setup, find_packages
+import re
+from pip.req import parse_requirements
+import uuid
+
+
+requirements = parse_requirements('requirements.txt', session=uuid.uuid1())
+install_requires = [str(ir.req) for ir in requirements if not ir.url]
+
+setup(
+    name='nekbot.plugins.random',
+    namespace_packages = ['nekbot.plugins'],
+    version='1.0',
+
+    description='Randomness plugin for Nekbot, a modular multiprotocol bot.',
+
+    author='Nekmo',
+    author_email='contacto@nekmo.com',
+
+    url='https://bitbucket.org/Nekmo/nekbot.plugins.random',
+    
+    download_url='https://bitbucket.org/Nekmo/nekbot.plugins.random/get/default.tar.gz',
+
+    classifiers=[
+        'Natural Language :: Spanish',
+        'Development Status :: 1 - Planning',
+        'License :: OSI Approved :: MIT License',
+        'Intended Audience :: Developers',
+        'Environment :: Console',
+        'Topic :: Communications :: Chat',
+        'Topic :: Communications :: Chat :: Internet Relay Chat',
+        'Topic :: Communications :: Conferencing',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+
+    platforms=['linux'],
+
+    scripts=[],
+    
+    provides=['nekbot.plugins.random',
+              ],
+    
+    install_requires=install_requires,
+
+    packages=['nekbot', 'nekbot.plugins', 'nekbot.plugins.random'],
+    include_package_data=True,
+
+    keywords=['nekbot', 'bot', 'random', 'plugins', 'chat'],
+
+    entry_points={
+        'nekbot.plugins': [
+            'random = nekbot.plugins.random',
+        ],
+    },
+
+    zip_safe=False,
+)
